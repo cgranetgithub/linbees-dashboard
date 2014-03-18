027141e79d5b66476ffc409176869fac817a7117
charles <c.granet@gmail.com>
2014-03-18 11:20:15 +0100

    set server timezone to UTC, more optimized and more simple

--- service/settings/prod.py
+++ service/settings/prod.py
@@ -84,7 +84,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Europe/Paris'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
