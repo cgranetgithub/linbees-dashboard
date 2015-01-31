aaa6375fe5f03898ffa83eb4cc1b8495e0d93f20
charles <c.granet@gmail.com>
2015-01-31 16:53:04 +0100

    don't create bucket automatically => need to create my own with proper region (EU)

--- service/settings/s3_storage.py
+++ service/settings/s3_storage.py
@@ -11,7 +11,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
-AWS_AUTO_CREATE_BUCKET = True
+AWS_AUTO_CREATE_BUCKET = False
 AWS_HEADERS = {
     "Cache-Control": "public, max-age=86400",
 }
